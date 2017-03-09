c9baf807af58e13d90dbafcf6aa41a5c5d91a70b
Sakeeb <sakeeb.hossain1@gmail.com>
2017-03-08 23:10:30 -0500

    Script that pulls posts from https://www.facebook.com/groups/utsc2018/

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import simplejson as json
 from access_tokens import APP_ID
 from access_tokens import SECRET_KEY 
 from access_tokens import GROUP_ID 
+
 # Using the format specified on this page https://developers.facebook.com/docs/facebook-login/access-tokens/
 # under "App Access Tokens"
-api_id = APP_ID
-secret_key = SECRET_KEY
-access_token = api_id + "|" + secret_key
-
+access_token = APP_ID + "|" + SECRET_KEY
 url = "https://graph.facebook.com/" + GROUP_ID + "/feed?access_token=" + access_token
 
+# Retrieve JSON response.
 res = requests.get(url)
 json_posts = json.loads(str(res.text))
-print(json_posts)
-
